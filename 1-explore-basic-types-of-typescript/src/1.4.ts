e93ef91505e218ede9b0aab4cd95241e83e80b9d
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-03 18:51:05 +0600

    watched l-1.4: tuple type leatned

---
+++ 1-explore-basic-types-of-typescript/src/1.4.ts
@@ -0,0 +1,15 @@
+let firstName: string = "Tamim";
+
+let roll: number = 123;
+
+let isAdmin: boolean = true;
+
+let x: undefined = undefined;
+
+let y: null = null;
+
+let d;
+
+// tuple
+
+let ageNAme: [number, string] = [2, "hello"];
