8d6a728bc607b1357bda644bb504a88e059c768e
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-03 19:07:52 +0600

    watched L-1-5:learned abot literal types,optional types,readOnly types:

---
+++ 1-explore-basic-types-of-typescript/src/1.5.ts
@@ -0,0 +1,11 @@
+// Reference type --> Object
+
+const user: {
+  readonly company: "Programming Hero";
+  firstName: string; //literal type
+  middleName?: string; //optional type
+} = {
+  company: "Programming Hero",
+  firstName: "Tamim",
+  middleName: "Kamal",
+};
