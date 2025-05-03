69b7e4b81b6905610f37bd20ec1346c111e0b05e
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-03 20:29:21 +0600

    watched lecture 1.9: learned about type alias

---
+++ 1-explore-basic-types-of-typescript/src/1.9.ts
@@ -0,0 +1,20 @@
+{
+  //type alias
+  type Student = {
+    name: string;
+    age: number;
+    contact?: string;
+    gender: string;
+    address: string;
+  };
+
+  const student1: Student = {
+    name: "tamim",
+    age: 20,
+    gender: "Male",
+    address: "Dhaka",
+  };
+  console.log(student1)
+
+  //
+}
