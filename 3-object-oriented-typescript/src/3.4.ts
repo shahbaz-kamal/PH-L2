6649b495f8585731c7f31d77710a3d8aa869c8a4
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-07 20:17:52 +0600

    watched lecture 3.4: learned about type guarding using instance of

---
+++ 3-object-oriented-typescript/src/3.4.ts
@@ -0,0 +1,44 @@
+{
+  //instance of guard
+  class Animal {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+    makeSound() {
+      console.log("I am normal animal");
+    }
+  }
+  class Dog extends Animal {
+    constructor(name: string, age: number) {
+      super(name, age);
+    }
+    makeBark() {
+      console.log(`I am ${this.name}. my age is ${this.age}`);
+    }
+  }
+
+  class Cat extends Animal {
+    constructor(name: string, age: number) {
+      super(name, age);
+    }
+    makeMeow() {
+      console.log(`I am ${this.name}. my age is ${this.age}`);
+    }
+  }
+
+  const getAnimal = (animal: Animal) => {
+    if (animal instanceof Dog) {
+      animal.makeBark(); ////we cant accsess makeBark  here cause this scope know  animal is dog
+    } else if (animal instanceof Cat) {
+      animal.makeMeow(); ////we cant accsess get meow here cause this scope know  animal is cat
+    } else animal.makeSound(); ////we cant accsess make bark here cause this scope dosent know if animal is dog or cat
+  };
+  const dog = new Dog("dog", 20);
+  const cat = new Cat("cat", 2);
+  // console.log({ cat });
+  getAnimal(cat);
+}
