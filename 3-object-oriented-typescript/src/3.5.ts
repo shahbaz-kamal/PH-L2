fc37938e082a4166765b0759a625e313308800c2
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-07 20:59:48 +0600

    watched Lecture 3-5: learned about access modifier

---
+++ 3-object-oriented-typescript/src/3.5.ts
@@ -0,0 +1,26 @@
+{
+  //?access Modifier
+
+  class BankAccount {
+    public readonly id: number;
+    public name: string;
+    protected _balance: number; ////so that we cant access balance property outside of class. A convention ('underscore before name') is followed to indicate private property. Also to get access of this property by using extends in another class we define it protected instead of private
+
+    constructor(id: number, name: string, balance: number) {
+      this.id = id;
+      this.name = name;
+      this._balance = balance;
+    }
+
+    public addDeposite(amount: number) {
+      this._balance = this._balance + amount;
+    }
+  }
+  class Student extends BankAccount {
+    test() {}
+  }
+
+  const gorib = new BankAccount(1, "poor mia", 60);
+  gorib.addDeposite(3);
+  console.log(gorib);
+}
