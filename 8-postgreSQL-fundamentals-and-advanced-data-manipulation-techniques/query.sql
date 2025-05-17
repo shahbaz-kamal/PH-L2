7bf73e0d2082ab39dc79b74134075082d7b0e44b
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-18 01:12:45 +0600

    atched Lecture 8.2: learned about aLTER

--- 8-postgreSQL-fundamentals-and-advanced-data-manipulation-techniques/query.sql
+++ 8-postgreSQL-fundamentals-and-advanced-data-manipulation-techniques/query.sql
@@ -1,1 +1,21 @@
 SELECT * from person2
+
+alter table person2 
+ add column email varchar(25) DEFAULT 'default@mail.com' not null;
+
+--  inserting column
+
+INSERT INTO person2 values(4,'test2',45,'test2@gmail.com')
+
+ALTER Table person2 drop email
+
+alter table person2
+    RENAME column age to user_age
+
+alter table person2 
+    alter column username type varchar(51)
+
+alter table person2
+    alter COLUMN user_age set not null;
+alter table person2
+    alter COLUMN user_age drop not null;
