17c0786b0645ed6beb454a0244459159820a5a5c
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-21 20:29:44 +0600

    watched lecture 9-3: learned about reffering foreighn key

---
+++ 9-managing-dates,-relationships-and-understanding-JOIN-in-postgreSQL/foreign_key.sql
@@ -0,0 +1,45 @@
+
+-- creating user table
+create Table "user" (
+id SERIAL PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+age int not NULL
+)
+
+create Table post (
+    id SERIAL PRIMARY KEY,
+    title text NOT NULL,
+    posted_date DATE NOT NULL,
+    user_id Int REFERENCES "user"(id)
+)
+
+INSERT INTO "user" (name,age) VALUES
+('Alice', 25),
+('Bob', 30),
+('Charlie', 28),
+('Diana', 22),
+('Ethan', 35);
+
+INSERT INTO post (title,posted_date,user_id) VALUES
+    ('Alice shares her journey in learning SQL', '2025-05-01', 1),
+('Alice discusses best practices for SQL joins', '2025-05-02', 1),
+
+('Bob explains JavaScript basics for beginners', '2025-05-03', 2),
+('Bob explores advanced React patterns', '2025-05-04', 2),
+
+('Charlie writes about designing REST APIs', '2025-05-05', 3),
+
+('Diana compares CSS Grid and Flexbox', '2025-05-06', 4),
+
+('Ethan introduces Docker for beginners', '2025-05-07', 5),
+('Ethan talks about database normalization', '2025-05-08', 5);
+
+
+drop table "user"
+
+DROP table post
+
+SELECT * from "user"
+
+SELECT *,name from post
+    
--- 9-managing-dates,-relationships-and-understanding-JOIN-in-postgreSQL/groupBy.sql
+++ 9-managing-dates,-relationships-and-understanding-JOIN-in-postgreSQL/groupBy.sql
@@ -1,4 +1,11 @@
 select * from student
 
-select country from student
-group by country
+select country,count(*), avg(age) from student
+group by country 
+
+SELECT country,count(*),avg(age) from student
+GROUP BY country
+HAVING avg(age)>20.5;
+
+SELECT extract(year from dob) as birth_year,count(*) FROM student
+    GROUP BY birth_year;
