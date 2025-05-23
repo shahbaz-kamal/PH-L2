645f3545949b7bbde3a36a6376a08d8b4622b522
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-23 20:25:28 +0600

    watched l 10-1

--- 9-managing-dates,-relationships-and-understanding-JOIN-in-postgreSQL/practise.sql
+++ 9-managing-dates,-relationships-and-understanding-JOIN-in-postgreSQL/practise.sql
@@ -71,4 +71,36 @@ SELECT extract(year FROM hire_date) as hire_year,count(*) from employee e
     JOIN department d ON e.department_id=d.id
     GROUP BY hire_year
 
+select * from employee
 
+select * from department
+
+CREATE Table "order" (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INT,
+    order_date Date,
+    total_amount DECIMAL(10,2)
+)
+
+drop table "order"
+
+INSERT INTO "order" (customer_id, order_date, total_amount) VALUES
+(101, '2023-01-10', 250.75),
+(102, '2023-02-14', 125.00),
+(103, '2023-03-20', 480.50),
+(104, '2023-04-05', 320.00),
+(105, '2023-05-15', 150.00),
+(101, '2023-06-18', 275.30),
+(106, '2023-07-21', 410.90),
+(102, '2023-08-09', 190.00),
+(107, '2023-09-12', 360.45),
+(108, '2023-10-25', 510.00);
+
+select customer_id, count(*), sum(total_amount) from "order"
+    GROUP BY customer_id
+    HAVING count(*)>2
+
+SELECT extract(month from order_date) as order_month, count(*),sum(total_amount) as total_tk from "order"
+WHERE extract(year from order_date)=2023
+GROUP BY order_month
+ORDER BY order_month  
