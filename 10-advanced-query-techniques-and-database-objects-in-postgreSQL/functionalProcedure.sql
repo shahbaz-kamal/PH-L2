de3f5abb627f98f61e410a506dbac02ecf6e80df
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-24 02:06:04 +0600

    watched lecture 10-5: learned about functions

---
+++ 10-advanced-query-techniques-and-database-objects-in-postgreSQL/functionalProcedure.sql
@@ -0,0 +1,33 @@
+select * from employee
+
+
+CREATE Function emp_count()
+RETURNS INT
+LANGUAGE SQL
+AS
+$$
+SELECT count(*) from employee
+$$
+
+create FUNCTION delete_emp()
+RETURNS void
+LANGUAGE SQL
+AS
+$$
+DELETE from employee WHERE employee_id=30
+$$
+
+CREATE Function delete_by_params(p_emp_id int)
+RETURNS void
+LANGUAGE SQL
+AS
+$$
+DELETE from employee where employee_id=p_emp_id
+$$
+
+
+select emp_count()
+
+SELECT delete_emp()
+
+select delete_by_params(29)
