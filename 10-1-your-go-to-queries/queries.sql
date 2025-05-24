a3f55e8681ba9466c29097460f2dae033ff9ef1a
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-25 01:25:50 +0600

    practised Grouping

--- 10-1-your-go-to-queries/queries.sql
+++ 10-1-your-go-to-queries/queries.sql
@@ -36,3 +36,11 @@ SELECT * FROM books LIMIT 10 OFFSET 10
 SELECT * from books
 
 UPDATE books SET price=price*1.1;
+
+-- //Grouping
+
+SELECT * FROM books
+
+SELECT author,COUNT(*) FROM books GROUP BY author
+
+SELECT author,count(*) FROM books GROUP BY author HAVING count(*)>2
