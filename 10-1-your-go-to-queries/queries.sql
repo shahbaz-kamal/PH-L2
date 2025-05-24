1f3e8519a0f4e5258d97b676f85b0655501dd622
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-24 22:35:21 +0600

    practised BETWEE, LIKE, ILIKE and IN

--- 10-1-your-go-to-queries/queries.sql
+++ 10-1-your-go-to-queries/queries.sql
@@ -13,3 +13,15 @@ SELECT title, LENGTH(title) from ;
 SELECT avg(price) from books
 
 SELECT MAX(Price),min(price) from books
+
+-- //BETWEEN , LIKE, IN, ILIKE
+
+select * from books
+
+select * from books WHERE price BETWEEN 10 AND 12
+
+SELECT * FROM books WHERE author LIKE '%Orwell'
+
+SELECT * FROM books WHERE title ILIKE 'the%'
+
+SELECT * FROM books WHERE author IN ('Colleen Hoover','George Orwell ')
