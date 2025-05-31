79932d230d1495e8244be805d60448817f36657a
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-31 14:41:49 +0600

    watched lecture 12-9: learned more about commonjs module

---
+++ 12-0-explore-the-fundamentals-of-node.js/file-1.js
@@ -0,0 +1,3 @@
+const var1=require("./file-2.js");
+console.log(var1.a)
+console.log(var1.add(10,20));
---
+++ 12-0-explore-the-fundamentals-of-node.js/file-2.js
@@ -0,0 +1,9 @@
+const a = 10;
+const b = 10;
+const add = (param1, param2) => {
+  return param1 + param2;
+};
+
+module.exports = {add,a};
+
+// console.log(module);
