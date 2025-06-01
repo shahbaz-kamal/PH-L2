87a70ddf217383578b07936263f005052b9698c2
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-06-01 23:24:56 +0600

    learned about aliasing

--- 12-0-explore-the-fundamentals-of-node.js/file-1.js
+++ 12-0-explore-the-fundamentals-of-node.js/file-1.js
@@ -1,3 +1,7 @@
-const var1=require("./file-2.js");
-console.log(var1.a)
-console.log(var1.add(10,20));
+const {a,add,b}=require("./file-2.js");
+
+const {a:a3,add:add3,b:b3}=require("./file-3.js")
+
+console.log(a3)
+console.log(add3(10,20,30));
+console.log(b)
--- 12-0-explore-the-fundamentals-of-node.js/file-3.js
+++ 12-0-explore-the-fundamentals-of-node.js/file-3.js
@@ -1,7 +1,7 @@
 const a = 10;
 const b = 10;
-const add = (param1, param2) => {
-  return param1 + param2;
+const add = (param1, param2,param3) => {
+  return param1 + param2+param3;
 };
 
 module.exports = {add,a,b};
