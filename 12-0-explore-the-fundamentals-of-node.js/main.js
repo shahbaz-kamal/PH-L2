58cd7bd14ac4a903a30f25891a03fd96458d745a
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-06-01 23:34:18 +0600

    learned mor about aliasing

---
+++ 12-0-explore-the-fundamentals-of-node.js/main.js
@@ -0,0 +1,5 @@
+const {add,subtract}=require('./utils')
+
+
+console.log(add(3,2))
+console.log(subtract(3,2))
---
+++ 12-0-explore-the-fundamentals-of-node.js/utils/subtract.js
@@ -0,0 +1,7 @@
+const subtract = (param1, param2) => {
+  return param1 - param2;
+};
+
+module.exports = {
+  subtract,
+};
