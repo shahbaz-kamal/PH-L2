69ea4964c2a741b1eabdc5817ed65b0ddc276329
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-06-03 02:39:25 +0600

    watched 13-7: learned about basic routing through postman

--- 13-0-explore-node.js-core-modules/toDo-App/server.js
+++ 13-0-explore-node.js-core-modules/toDo-App/server.js
@@ -2,10 +2,25 @@ const http = require("http");
 const port = 5000;
 
 const server = http.createServer((req, res) => {
-  console.log({ req, res });
-  res.end("Welcome to ToDo App");
+  //   console.log( req.url, req.method );
+  //   res.end("Welcome to ToDo App");
+
+  if (req.url === "/todos" && req.method === "GET") {
+    res.end("ToDO got");
+  } else if (req.url === "/todos/create-todo" && req.method === "POST") {
+    res.end("ToDo created");
+  } else {
+    res.end("Route NOT Found");
+  }
 });
 
 server.listen(port, "127.0.0.1", () => {
   console.log("✅ Server is listening to port 5000");
 });
+
+/**
+ * /todos - GET -All todos
+ * /todos/create-todo Post create to
+ *
+ *
+ */
