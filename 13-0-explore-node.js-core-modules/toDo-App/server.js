0d160f25d2034ad1cc27635019c1c3f10c2f2305
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-06-04 18:45:36 +0600

    watched L-13-10: learned about creating todos

--- 13-0-explore-node.js-core-modules/toDo-App/server.js
+++ 13-0-explore-node.js-core-modules/toDo-App/server.js
@@ -1,26 +1,44 @@
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
 const port = 5000;
 
-const data = [
-  { "title": "prisma", "body": "learning node", "createdAt": "5/18/2025,1:25:12 AM" },
-  { "title": "typescript", "body": "learning node", "createdAt": "5/18/2025,1:25:02 AM" },
-];
+const filePath = path.join(__dirname, "./db/todo.json");
 
 const server = http.createServer((req, res) => {
   //   console.log( req.url, req.method );
   //   res.end("Welcome to ToDo App");
-
+  const data = fs.readFileSync(filePath, { encoding: "utf-8" });
   if (req.url === "/todos" && req.method === "GET") {
-    res.writeHead(201, {
-      "content-type": "text/html",
-      email: "a@b.com",
+    res.writeHead(200, {
+      "content-type": "application/json",
     });
     // res.setHeader("contemt-type","text/plain")
     // res.setHeader("email","t@p.com")
     // res.statusCode=200
-    res.end(`<h1>Hello World</h1>`);
-  } else if (req.url === "/todos/create-todo" && req.method === "POST") {
-    res.end("ToDo created");
+    res.end(data);
+  }
+  // createing todo
+  else if (req.url === "/todos/create-todo" && req.method === "POST") {
+    let data = "";
+    req.on("data", (chunk) => {
+      data = data + chunk;
+    });
+
+    req.on("end", () => {
+      const { title, body } = JSON.parse(data);
+      console.log({ title, body });
+      const createdAt = new Date().toLocaleString();
+      const allTodos = fs.readFileSync(filePath, { encoding: "utf-8" });
+      console.log(allTodos);
+      const parsedTodos = JSON.parse(allTodos);
+      parsedTodos.push({ title, body, createdAt });
+      fs.writeFileSync(filePath, JSON.stringify(parsedTodos,null,2), {
+        encoding: "utf-8",
+      });
+      res.end(JSON.stringify({ title, body, createdAt },null,2));
+    });
+    // const allTodos = fs.readFileSync(filePath, { encoding: "utf-8" });
   } else {
     res.end("Route NOT Found");
   }
@@ -28,6 +46,7 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, "127.0.0.1", () => {
   console.log("✅ Server is listening to port 5000");
+  // console.log(filePath)
 });
 
 /**
