1c46f855bb9232c2ad6de980935ff7957ebdac78
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-06-07 02:26:52 +0600

    watched lecture 13-11

--- 13-0-explore-node.js-core-modules/toDo-App/server.js
+++ 13-0-explore-node.js-core-modules/toDo-App/server.js
@@ -7,11 +7,11 @@ const port = 5000;
 const filePath = path.join(__dirname, "./db/todo.json");
 
 const server = http.createServer((req, res) => {
-    // console.log( req.url, req.method );
+  // console.log( req.url, req.method );
   //   res.end("Welcome to ToDo App");
-  const url=new URL(req.url,`http://${req.headers.host}`)
-  const pathname=url.pathname
-console.log(url.pathname)
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const pathname = url.pathname;
+  console.log(url.pathname);
 
   // console.log(`The requested urll is ${req.url}`)
   // console.log(`The HTTP URL IS ${req.headers.host}`)
@@ -40,23 +40,51 @@ console.log(url.pathname)
       console.log(allTodos);
       const parsedTodos = JSON.parse(allTodos);
       parsedTodos.push({ title, body, createdAt });
-      fs.writeFileSync(filePath, JSON.stringify(parsedTodos,null,2), {
+      fs.writeFileSync(filePath, JSON.stringify(parsedTodos, null, 2), {
         encoding: "utf-8",
       });
-      res.end(JSON.stringify({ title, body, createdAt },null,2));
+      res.end(JSON.stringify({ title, body, createdAt }, null, 2));
     });
     // const allTodos = fs.readFileSync(filePath, { encoding: "utf-8" });
-  } 
-else if(pathname==="/todo" && req.method==="GET"){
-const title=url.searchParams.get("title")
-console.log(title)
-const data = fs.readFileSync(filePath, { encoding: "utf-8" });
-const parsedData=JSON.parse(data)
-const todo=parsedData.find(todo=>todo.title===title)
-const stringifiedTodo=JSON.stringify(todo)
-  res.end(stringifiedTodo)
-}  
-  else {
+  } else if (pathname === "/todo" && req.method === "GET") {
+    const title = url.searchParams.get("title");
+    console.log(title);
+    const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+    const parsedData = JSON.parse(data);
+    const todo = parsedData.find((todo) => todo.title === title);
+    const stringifiedTodo = JSON.stringify(todo);
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+    res.end(stringifiedTodo);
+  } else if (pathname === "/todos/update-todo" && req.method === "PATCH") {
+    const title = url.searchParams.get("title");
+    let data = "";
+    req.on("data", (chunk) => {
+      data = data + chunk;
+    });
+
+    req.on("end", () => {
+      const { body } = JSON.parse(data);
+
+      const allTodos = fs.readFileSync(filePath, { encoding: "utf-8" });
+
+      const parsedTodos = JSON.parse(allTodos);
+      const todoIndex = parsedTodos.findIndex((todo) => todo.title === title);
+      parsedTodos[todoIndex].body = body;
+      fs.writeFileSync(filePath, JSON.stringify(parsedTodos, null, 2), {
+        encoding: "utf-8",
+      });
+      res.end(
+        JSON.stringify(
+          { title, body, createdAt: parsedTodos[todoIndex].createdAt },
+          null,
+          2
+        )
+      );
+    });
+    // const allTodos = fs.readFileSync(filePath, { encoding: "utf-8" });
+  } else {
     res.end("Route NOT Found");
   }
 });
