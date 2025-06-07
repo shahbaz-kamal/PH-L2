22ed03a1341f3e7b2eb86e16a85499e50ef5d844
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-06-08 00:33:47 +0600

    watched lecture 14-3

--- 14-0-mastering-the-foundation-of-express/dist/app.js
+++ 14-0-mastering-the-foundation-of-express/dist/app.js
@@ -4,8 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const filePath = path_1.default.join(__dirname, "../db/todo.json");
 app.get("/", (req, res) => {
-    res.send("To do app is running");
+    res.send("To do app is running asdfadfg");
+});
+app.get("/todos", (req, res) => {
+    const data = fs_1.default.readFileSync(filePath, { encoding: "utf-8" });
+    console.log(data);
+    res.json(data);
+});
+app.post("/todos/create-todo", (req, res) => {
+    const { title, body } = req.body;
+    console.log(title, body);
 });
 exports.default = app;
--- 14-0-mastering-the-foundation-of-express/src/app.ts
+++ 14-0-mastering-the-foundation-of-express/src/app.ts
@@ -1,12 +1,26 @@
 import express, { Application, Request, Response } from "express";
+import fs from "fs";
+import path from "path";
 
 const app: Application = express();
 
+app.use(express.json());
+
+const filePath = path.join(__dirname, "../db/todo.json");
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("To do app is running");
+  res.send("To do app is running asdfadfg");
 });
 
+app.get("/todos", (req: Request, res: Response) => {
+  const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+  console.log(data);
+  res.json(data);
+});
 
+app.post("/todos/create-todo", (req: Request, res: Response) => {
+  const { title, body } = req.body;
+  console.log(title, body);
+});
 
 export default app;
