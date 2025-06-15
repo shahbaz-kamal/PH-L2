47748a3fbf69684293fc2aeb24133aa95153eba7
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-06-16 01:55:14 +0600

    watched 14-11:learned advanced error handling in express

--- 14-0-mastering-the-foundation-of-express/dist/app.js
+++ 14-0-mastering-the-foundation-of-express/dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,5 +24,25 @@ console.log(filePath);
 app.use("/todos", todos_routes_1.todosRouter);
 app.get("/", (req, res) => {
     res.send("To do app is running asdfadfg");
+});
+app.get("/error", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        console.log("hello");
+        res.send("welcome to error er duniya");
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+app.use((req, res) => {
+    res.status(404).send({ message: "Route not found" });
+}); // Handle 404 errors
+app.use((error, req, res, next) => {
+    if (error) {
+        console.log("eroor", error);
+        res
+            .status(500)
+            .json({ message: "Internal server error", error: error.message });
+    }
 });
 exports.default = app;
--- 14-0-mastering-the-foundation-of-express/src/app.ts
+++ 14-0-mastering-the-foundation-of-express/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import fs from "fs";
 import path from "path";
 import { todosRouter } from "./app/todos/todos.routes";
@@ -16,6 +16,28 @@ app.use("/todos", todosRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("To do app is running asdfadfg");
+});
+
+app.get("/error", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    console.log("hello");
+    res.send("welcome to error er duniya");
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((req: Request, res: Response) => {
+  res.status(404).send({ message: "Route not found" });
+}); // Handle 404 errors
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  if (error) {
+    console.log("eroor", error);
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
 });
 
 export default app;
