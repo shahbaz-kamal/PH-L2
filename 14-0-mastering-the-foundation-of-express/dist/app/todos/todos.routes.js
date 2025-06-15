8a16dd90f339945149bcc49353587aa647003a4c
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-06-16 01:16:00 +0600

    watched 14-8

--- 14-0-mastering-the-foundation-of-express/dist/app/todos/todos.routes.js
+++ 14-0-mastering-the-foundation-of-express/dist/app/todos/todos.routes.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,6 +16,7 @@ exports.todosRouter = void 0;
 const fs_1 = __importDefault(require("fs"));
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
+const clientDB_1 = require("../utils/clientDB");
 exports.todosRouter = express_1.default.Router();
 const filePath = path_1.default.join(__dirname, "../../../db/todo.json");
 console.log(filePath);
@@ -16,11 +26,18 @@ exports.todosRouter.get("/", (req, res) => {
     // res.json(data);
     res.send({ Message: "todos router", data });
 });
-exports.todosRouter.post("/create-todo", (req, res) => {
-    const { title, body } = req.body;
-    console.log(title, body);
-    res.send({ message: "todo created", title, body });
-});
+exports.todosRouter.post("/create-todo", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(req.body);
+    const newTodo = {
+        title: req.body.title,
+        description: req.body.description,
+        priority: req.body.priority,
+        isCompleted: req.body.isCompleted,
+        createdAt: new Date().toLocaleDateString(),
+    };
+    const result = yield clientDB_1.todoCollection.insertOne(req.body);
+    res.send(result);
+}));
 exports.todosRouter.get("/:title", (req, res) => {
     const { title, body } = req.body;
     console.log(title, body);
--- 14-0-mastering-the-foundation-of-express/src/app/todos/todos.routes.ts
+++ 14-0-mastering-the-foundation-of-express/src/app/todos/todos.routes.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import express, { Request, Response } from "express";
 import path from "path";
+import { todoCollection } from "../utils/clientDB";
 
 export const todosRouter = express.Router();
 const filePath = path.join(__dirname, "../../../db/todo.json");
@@ -13,10 +14,19 @@ todosRouter.get("/", (req: Request, res: Response) => {
   res.send({ Message: "todos router", data });
 });
 
-todosRouter.post("/create-todo", (req: Request, res: Response) => {
-  const { title, body } = req.body;
-  console.log(title, body);
-  res.send({ message: "todo created", title, body });
+todosRouter.post("/create-todo", async (req: Request, res: Response) => {
+  console.log(req.body);
+const newTodo={
+    title: req.body.title,
+    description: req.body.description,
+    priority: req.body.priority,
+    isCompleted: req.body.isCompleted,
+    createdAt: new Date().toLocaleDateString(),
+  
+  };
+ 
+  const result = await todoCollection.insertOne(req.body);
+  res.send(result);
 });
 
 todosRouter.get("/:title", (req: Request, res: Response) => {
