397eea72ee05a6be2ca44053331a69d0dcb7725f
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-06-17 03:24:59 +0600

    watched lecture 15-7: learned about ,

--- 15-0-in-depth-exploration-of-mongodb-queries/log.js
+++ 15-0-in-depth-exploration-of-mongodb-queries/log.js
@@ -2,3 +2,4 @@
 //watched lecture 15-4: learned about $in (implicit and)
 //watched lecture 15-5: learned about explicit  $and,$or
 //watched lecture 15-6: learned about $exists,$type,$size
+//watched lecture 15-7: learned about $ALL,$elemMatch
