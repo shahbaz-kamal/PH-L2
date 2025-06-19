56dde33d381feb1043314ed01d764653bb7e066c
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-06-19 18:32:50 +0600

    watched 15-2: Learned about the .  and  stage

--- 16-0-mastering-mongoDB-aggregation-&-indexing/log.js
+++ 16-0-mastering-mongoDB-aggregation-&-indexing/log.js
@@ -1,2 +1,3 @@
 // watched 15-0: Got an intro about aggregation framework
 // watched 15-1: Learned about the $match stage and $project stage
+// watched 15-2: Learned about the $AddFields. $out and $Merge stage
