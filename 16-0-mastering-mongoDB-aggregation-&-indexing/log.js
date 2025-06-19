90464f6e14a817bf0cae927c7693b9c34b6d9cf2
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-06-19 19:52:07 +0600

    watched 15-5: Learned about the  stage

--- 16-0-mastering-mongoDB-aggregation-&-indexing/log.js
+++ 16-0-mastering-mongoDB-aggregation-&-indexing/log.js
@@ -3,3 +3,4 @@
 // watched 15-2: Learned about the $AddFields. $out and $Merge stage
 // watched 15-3: Learned about the $group stage
 // watched 15-4: Learned about the $sum,$min, $max, $avg,$substract and $push operators
+// watched 15-5: Learned about the $unwind stage
