423adfb9980cf6aac3d58a8fb0090dbd0a364949
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-06-19 21:53:06 +0600

    watched 16-8: Learned about the  stage (referencing documents)

--- 16-0-mastering-mongoDB-aggregation-&-indexing/log.js
+++ 16-0-mastering-mongoDB-aggregation-&-indexing/log.js
@@ -6,3 +6,4 @@
 // watched 16-5: Learned about the $unwind stage
 // watched 16-6: Learned about the $bucket stage
 // watched 16-7: Learned about the $pipeline stage
+// watched 16-8: Learned about the $lookup stage (referencing documents)
