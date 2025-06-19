187580523f9ef95871d6fc6d68d4a06f45647ac2
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-06-19 21:24:44 +0600

    watched 16-7: Learned about the  stage

--- 16-0-mastering-mongoDB-aggregation-&-indexing/log.js
+++ 16-0-mastering-mongoDB-aggregation-&-indexing/log.js
@@ -1,7 +1,8 @@
-// watched 15-0: Got an intro about aggregation framework
-// watched 15-1: Learned about the $match stage and $project stage
-// watched 15-2: Learned about the $AddFields. $out and $Merge stage
-// watched 15-3: Learned about the $group stage
-// watched 15-4: Learned about the $sum,$min, $max, $avg,$substract and $push operators
-// watched 15-5: Learned about the $unwind stage
-// watched 15-6: Learned about the $bucket stage
+// watched 16-0: Got an intro about aggregation framework
+// watched 16-1: Learned about the $match stage and $project stage
+// watched 16-2: Learned about the $AddFields. $out and $Merge stage
+// watched 16-3: Learned about the $group stage
+// watched 16-4: Learned about the $sum,$min, $max, $avg,$substract and $push operators
+// watched 16-5: Learned about the $unwind stage
+// watched 16-6: Learned about the $bucket stage
+// watched 16-7: Learned about the $pipeline stage
