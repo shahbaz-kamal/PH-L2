eb262a773d37c76408ea69fef8a2e60ffa35209a
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-07-12 18:07:11 +0600

    watched -

--- 22-bootstraping-redux-basic-project/src/module/tasks/AddTaskModal.tsx
+++ 22-bootstraping-redux-basic-project/src/module/tasks/AddTaskModal.tsx
@@ -39,17 +39,18 @@ import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
 import { addTask } from "@/redux/features/task/taskSlice";
 import { useAppDispatch } from "@/redux/hook";
+import type { ITask } from "@/types";
 import { format } from "date-fns";
 import { CalendarIcon } from "lucide-react";
-import { useForm } from "react-hook-form";
+import { useForm, type FieldValues, type SubmitHandler } from "react-hook-form";
 import { Link } from "react-router";
 
 const AddTaskModal = () => {
   const form = useForm();
   const dispatch = useAppDispatch();
-  const onSubmit = (data) => {
+  const onSubmit:SubmitHandler<FieldValues> = (data) => {
     console.log(data);
-    dispatch(addTask(data));
+    dispatch(addTask(data as ITask));
   };
   return (
     <Dialog>
--- 22-bootstraping-redux-basic-project/src/redux/features/task/taskSlice.ts
+++ 22-bootstraping-redux-basic-project/src/redux/features/task/taskSlice.ts
@@ -1,7 +1,6 @@
 import type { RootState } from "@/redux/store";
 import type { ITask } from "@/types";
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
-import {v4 as uuidv4} from 'uuid'
+import { createSlice, nanoid, type PayloadAction } from "@reduxjs/toolkit";
 
 interface InitialState {
   tasks: ITask[];
@@ -29,17 +28,19 @@ const initialState: InitialState = {
   ],
   filter: "all",
 };
+
+type DraftTask = Pick<ITask, "title" | "description" | "dueDate" | "priority">;
+
+const createTask = (taskData: DraftTask): ITask => {
+  return { id: nanoid(), isCompleted: false, ...taskData };
+};
+
 const taskSlice = createSlice({
   name: "task",
   initialState,
   reducers: {
     addTask: (state, action: PayloadAction<ITask>) => {
-      const id = uuidv4()
-      const taskData = {
-        ...action.payload,
-        id,
-        isCompleted: false,
-      };
+      const taskData = createTask(action.payload);
       state.tasks.push(taskData);
     },
   },
