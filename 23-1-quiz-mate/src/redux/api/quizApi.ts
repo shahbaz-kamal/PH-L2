54a1f06d9637f57905d756960692b91934ed202f
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-07-19 15:36:31 +0600

    fr 2: 1-26:40

---
+++ 23-1-quiz-mate/src/redux/api/quizApi.ts
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+export const quizApi = createApi({
+  reducerPath: "quizApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:5000/api",
+  }),
+  tagTypes: ["quiz"],
+  endpoints: (builder) => {
+    addQuiz: builder.mutation({
+      query: (newQuiz) => ({
+        url: "/quizzes",
+        method: "POST",
+        body: newQuiz,
+      }),
+      invalidatesTags: ["quiz"],
+    });
+  },
+});
