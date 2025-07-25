4daabdcdd23a0cb69191f3888072a515a1a03b21
Md. Shahbaz Kamal Chowdhury <shahbazkamal384@gmail.com>
2025-07-26 03:11:18 +0600

    26:45

--- 23-3-special-session-client/src/App.tsx
+++ 23-3-special-session-client/src/App.tsx
@@ -1,12 +1,9 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
-import Count from "./count/Count";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center space-x-2">
-      <Count></Count>
-      <Count></Count>
+      main fn
     </div>
   );
 }
