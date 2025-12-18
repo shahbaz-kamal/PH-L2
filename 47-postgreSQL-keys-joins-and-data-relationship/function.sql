82f8c0151bd090472a4b188fbddb0955e0c15489
Md. Shahbaz Kamal Chowdhury <shahbazkamal384@gmail.com>
2025-12-19 01:21:09 +0600

    watched 47-13

--- 47-postgreSQL-keys-joins-and-data-relationship/function.sql
+++ 47-postgreSQL-keys-joins-and-data-relationship/function.sql
@@ -21,3 +21,22 @@ create function delete_emp_by_Id(emp_id int)
  select * from employees
  
  select  delete_emp_by_id(2)
+
+
+create or replace procedure inc_low_salary_by_dept(dept varchar(50)) 
+LANGUAGE plpgsql
+as 
+$$
+declare 
+avg_salary int ;
+begin
+select avg(salary) into avg_salary from employees where department=dept;
+update employees set salary = salary * 1.1 where salary < avg_salary and department=dept;
+end
+$$
+
+call  inc_low_salary_by_dept('HR')
+
+select avg(salary) into avg_salary from employees where department='HR'
+
+select * from employees
