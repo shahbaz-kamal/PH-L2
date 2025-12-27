8a4b5c54e77eeb348cb067c3d2c4c175d0ac1e63
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-12-27 14:27:44 +0600

    add authentication

--- 53-nextjs-core-concepts-2/next-blog-ui/src/app/helpers/authOptions.ts
+++ 53-nextjs-core-concepts-2/next-blog-ui/src/app/helpers/authOptions.ts
@@ -1,5 +1,6 @@
 import { envVars } from "@/config/env";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -8,6 +9,62 @@ export const authOptions = {
       clientId: envVars.GOOGLE_CLIENT_ID as string,
       clientSecret: envVars.GOOGLE_CLIENT_SECRET as string,
     }),
+     CredentialsProvider({
+    // The name to display on the sign in form (e.g. "Sign in with...")
+    name: "Credentials",
+    // `credentials` is used to generate a form on the sign in page.
+    // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+    // e.g. domain, username, password, 2FA token, etc.
+    // You can pass any HTML attribute to the <input> tag through the object.
+    credentials: {
+      email: { label: "Email", type: "text"},
+      password: { label: "Password", type: "password" }
+    },
+    async authorize(credentials) {
+      // Add logic here to look up the user from the credentials supplied
+
+      if(!credentials?.email || !credentials.password) {
+        console.error("Email or password missing")
+        return null
+      }
+
+      try {
+          const res = await fetch(`${envVars.BASE_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({email:credentials.email,password:credentials.password}),
+  });
+
+  const user = await res.json();
+  if (!res.ok) {
+    return {
+      success: false,
+      message: result?.message || "Login failed",
+      result,
+    };
+  }
+
+     if (user) {
+        // Any object returned will be saved in `user` property of the JWT
+        return user
+      } else {
+        // If you return null then an error will be displayed advising the user to check their details.
+        return null
+
+        // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+      }
+      } catch (error) {
+        console.error(error)
+        return null
+      }
+     
+
+   
+    }
+  })
+]
   ],
   secret: envVars.AUTH_SECRET,
   pages:{
