17ba5f299e9a4522bab33a38622f269b4c475edd
shahbaz-kamal <shahbazkamal384@gmail.com>
2026-01-06 18:38:02 +0600

    watched 56-5

--- 56-health-care-server/src/app.ts
+++ 56-health-care-server/src/app.ts
@@ -2,6 +2,8 @@ import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import config from './config';
+import { timeStamp } from 'console';
 
 const app: Application = express();
 app.use(cors({
@@ -16,7 +18,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
     res.send({
-        Message: "Ph health care server.."
+        message: "health care server is running ..",
+        enviroment:config.node_env,
+        uptime:process.uptime().toFixed(2) + ' Seconds',
+        timeStamp:new Date().toISOString()
     })
 });
 
